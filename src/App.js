4a111bd7ce6e7b5e1f7bf3b7191a9174dad4d91f
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-19 09:48:33 +0300

    lohs

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import {useState} from "react"
 
 function App() {
 const [result, setResult] = useState(0);
+const [ratioChange, setRatioChange] = useState(0);
 
 function addRatio(){
-setResult(result + 1);
+setResult(result+parseInt(ratioChange));
 console.log("funkcija ir izsaukta");
 console.log("result ir " + result);
   }
   function minus(){
     setResult(result - 1);
   }
+  function handleRatioChange(kakens){
+    setRatioChange(kakens.target.value);
+  }
   return (
     <div className="App">
-      <input type="number" name="result"></input>
+      <input type="number" value={ratioChange} onChange={handleRatioChange} ></input>
     
       <button onClick={addRatio}>Gang +1 ratio</button>
       <button onClick={minus}>Gang -1 ratio</button>
+      <button onClick={subtractRatio}>Gang ratio enter</button>
 <h1>{result}</h1>
     </div>
   );
